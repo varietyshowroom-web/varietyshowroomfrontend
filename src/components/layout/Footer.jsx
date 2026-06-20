0f142b6de69e266152ff685fd02a5765e782f957
varietyshowroom-web <varietyshowroom@gmail.com>
2026-06-20 23:05:59 +0530

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -58,7 +58,7 @@ export const Footer = () => {
               </li>
               <li className="flex items-center">
                 <Phone size={20} className="text-maroon-light mr-3 shrink-0" />
-                <a href="tel:9182823269" className="text-white-bg/80 hover:text-maroon-light transition-colors">9182823269</a>
+                <a href="tel:9966606243" className="text-white-bg/80 hover:text-maroon-light transition-colors">9966606243</a>
               </li>
               <li className="flex items-center">
                 <Mail size={20} className="text-maroon-light mr-3 shrink-0" />
